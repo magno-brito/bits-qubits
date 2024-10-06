8ee1a845243059780e6cc08830d492d15062804a
magnobrito <magnoleal22@gmail.com>
2024-10-06 11:09:06 -0300

    Primeiro commit

---
+++ kings-and-pigs-main/js/utils.js
@@ -0,0 +1,33 @@
+
+
+
+Array.prototype.parse2D = function () {
+  const rows = []
+  for (let i = 0; i < this.length; i += 16) {
+    rows.push(this.slice(i, i + 16))
+  }
+
+  return rows
+}
+
+Array.prototype.createObjectsFrom2D = function () {
+  const objects = []
+  this.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+      if (symbol === 292 || symbol === 250) {
+        //adiciona um collision dentro collisionblocks array
+        objects.push(
+          new CollisionBlock({
+            position: {
+              x: x * 64,
+              y: y * 64,
+
+            },
+          })
+        )
+      }
+    })
+  })
+
+  return objects
+}
